25a635572e0a9477c78256689b78089bb4545ff3
Pacino3D <jan.verbancic@gmail.com>
2022-02-24 21:14:56 +0100

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var links = getElementsByClassName("menu");
+var menu = document.getElementById("menu");
+var links = menu.getElementsByClassName("menu-item");
 for (var i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
