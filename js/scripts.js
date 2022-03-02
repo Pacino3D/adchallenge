35b36d4d8ceccf67997ff55cc74c08986b89a94c
Pacino3D <jan.verbancic@gmail.com>
2022-03-03 00:13:57 +0100

    03.03.2022

--- js/scripts.js
+++ js/scripts.js
@@ -3,40 +3,26 @@ var eventtop = 0;
 var abouttop = 0;
 var aboutbot = 0;
 
-// window.addEventListener('load', () => {
-    
-//     console.log("abc:",item);
-//     const newstop = document.getElementById("block-adchallenge-content").getBoundingClientRect().top;
-//     const eventtop = document.getElementById("block-latest-event").getBoundingClientRect().top;
-//     const aboutop = document.getElementById("block-about-me").getBoundingClientRect().top;
-//     const aboutbot = document.getElementById("block-about-me").getBoundingClientRect().bottom;
-//     console.log(newstop);
-//     var scroll = window.getBoundingClientRect().top;
-//     console.log(scroll);
-//     // eventtop = $("#challenge-event")[0].getBoundingClientRect().top + $(window).scrollTop();
-//     // abouttop = $("#block-about-me")[0].getBoundingClientRect().top + $(window).scrollTop();
-//     // aboutbot = $("#block-about-me")[0].getBoundingClientRect().bottom + $(window).scrollTop();
-// });
 (function ($) {
 window.addEventListener("load", () => {
+    $(".site-slogan")[0].innerHTML = "<strong>Music</strong>Theme";
     $(".menu-item")[1].childNodes[1].href = "http://localhost/#block-adchallenge-content";
     newstop = $("#block-adchallenge-content")[0].getBoundingClientRect().top + $(window).scrollTop();
     eventtop = $("#challenge-event")[0].getBoundingClientRect().top + $(window).scrollTop();
     abouttop = $("#block-about-me")[0].getBoundingClientRect().top + $(window).scrollTop();
     aboutbot = $("#block-about-me")[0].getBoundingClientRect().bottom + $(window).scrollTop();  
 }); 
-
-// var items = $(".menu-item");
-// for (var i = 0; i < items.length; i++) {
-//       items[i].addEventListener("click", function() {
-//         var current = $(".active");
-//         for (var j = 0; j < current.length; j ++)
-//           current[j].className = current[j].className.replace(" active", "");
-//         this.className += " active";
-//       });
-//     }
+/* var items = $(".menu-item");
+for (var i = 0; i < items.length; i++) {
+      items[i].addEventListener("click", function() {
+        var current = $(".active");
+        for (var j = 0; j < current.length; j ++)
+          current[j].className = current[j].className.replace(" active", "");
+        this.className += " active";
+      });
+    } */
 window.addEventListener("scroll", () => {   
-    var scroll = $(window).scrollTop() + 130;
+    var scroll = $(window).scrollTop() + 150;
         if (scroll < newstop) {
             $(".menu-item")[0].className = "menu-item active";
         } else {
