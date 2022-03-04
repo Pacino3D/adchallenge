a27d1d2a258961a66158189fe9e2fe5af613b61f
Pacino3D <jan.verbancic@gmail.com>
2022-03-04 17:19:39 +0100

    04.03.2022

--- js/scripts.js
+++ js/scripts.js
@@ -2,53 +2,49 @@ var newstop = 0;
 var eventtop = 0;
 var abouttop = 0;
 var aboutbot = 0;
+var limits = [];
 
 (function ($) {
+    function UpdateScroll(scroll) {
+        for (var i = 0; i < limits.length; i ++) {
+            if (((limits[i][0] == undefined) || (scroll >= limits[i][0])) &&
+                ((limits[i][1] == undefined) || (scroll < limits[i][1]))) {
+                $(".menu-item").eq(i).addClass ("active");
+            } else {
+                $(".menu-item").eq(i).removeClass ("active");
+            }
+        }
+    }
 window.addEventListener("load", () => {
     $(".site-slogan")[0].innerHTML = "<strong>Music</strong>Theme"; /*Popravek za izgled na logotu <strong značka>*/
     $(".menu-item")[1].childNodes[1].href = "http://localhost/#block-adchallenge-content"; /*Popravek da deluje news gumb>*/
-    $(".menu-item")[0].className = "menu-item active";
-    newstop = $("#block-adchallenge-content")[0].getBoundingClientRect().top + $(window).scrollTop();
-    eventtop = $("#challenge-event")[0].getBoundingClientRect().top + $(window).scrollTop();
-    abouttop = $("#block-about-me")[0].getBoundingClientRect().top + $(window).scrollTop();
-    aboutbot = $("#block-about-me")[0].getBoundingClientRect().bottom + $(window).scrollTop();
-}); 
-window.addEventListener("resize", () => {
-    newstop = $("#block-adchallenge-content")[0].getBoundingClientRect().top + $(window).scrollTop();
-    eventtop = $("#challenge-event")[0].getBoundingClientRect().top + $(window).scrollTop();
-    abouttop = $("#block-about-me")[0].getBoundingClientRect().top + $(window).scrollTop();
-    aboutbot = $("#block-about-me")[0].getBoundingClientRect().bottom + $(window).scrollTop();
+    
+    newstop = $("#block-adchallenge-content").offset().top + $("#block-adchallenge-content").scrollTop();
+    eventtop = $("#block-latest-event").offset().top + $("#block-latest-event").scrollTop();
+    abouttop = $("#block-about-me").offset().top + $("#block-about-me").scrollTop();
+    aboutbot = $("#block-about-me").offset().bottom;
+    limits = [[undefined, newstop],
+              [newstop, eventtop],
+              [eventtop, abouttop],
+              [abouttop, aboutbot]];
+
+    UpdateScroll($(window).scrollTop());
 });
-/* var items = $(".menu-item");
-for (var i = 0; i < items.length; i++) {
-      items[i].addEventListener("click", function() {
-        var current = $(".active");
-        for (var j = 0; j < current.length; j ++)
-          current[j].className = current[j].className.replace(" active", "");
-        this.className += " active";
-      });
-    } */
+window.addEventListener("resize", () => {
+    newstop = $("#block-adchallenge-content").offset().top;
+    eventtop = $("#block-latest-event").offset().top;
+    abouttop = $("#block-about-me").offset().top;
+    aboutbot = $("#block-about-me").offset().bottom;
+    limits = [[undefined, newstop],
+              [newstop, eventtop],
+              [eventtop, abouttop],
+              [abouttop, aboutbot]];
+
+    UpdateScroll($(window).scrollTop());
+});
 window.addEventListener("scroll", () => {   
-    var scroll = $(window).scrollTop() + 120;
-        if (scroll < newstop) {
-            $(".menu-item")[0].className = "menu-item active";
-        } else {
-            $(".menu-item")[0].className = "menu-item";
-        }
-        if (scroll >= newstop & scroll < eventtop) {
-            $(".menu-item")[1].className = "menu-item active";
-        } else {
-            $(".menu-item")[1].className = "menu-item";
-        }
-        if (scroll >= eventtop & scroll < abouttop) {
-            $(".menu-item")[2].className = "menu-item active";
-        } else {
-            $(".menu-item")[2].className = "menu-item";
-        }
-        if (scroll >= abouttop & scroll < aboutbot) {
-            $(".menu-item")[3].className = "menu-item active";
-        } else {
-            $(".menu-item")[3].className = "menu-item";
-        }
+    var scroll = $(window).scrollTop() + $("header").outerHeight();
+
+    UpdateScroll(scroll);
   });
 })(jQuery);
